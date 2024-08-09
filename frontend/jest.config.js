3b3dc678ee40696cee3cf2382f516cb17fea6bc6
xopiie <46599014+NadicaUzunova@users.noreply.github.com>
2024-08-09 10:01:55 +0200

    Delete frontend/jest.config.js

--- frontend/jest.config.js
+++
@@ -1,7 +0,0 @@
-// jest.config.js
-module.exports = {
-  testMatch: ['**/*.test.js'],
-  transformIgnorePatterns: ['node_modules/(?!axios)/'],
-  moduleFileExtensions: ['js', 'jsx'],
-  testEnvironment: 'jsdom',
-};
